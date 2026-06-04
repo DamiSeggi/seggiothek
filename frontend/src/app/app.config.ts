cc42cb1f7d387d3926f324cdb23cc2b05a834242
DamiSeggi <damiansegginger@gmail.com>
2026-06-04 15:14:42 +0200

    Send "keycloak access token" with every request

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideKeycloak } from 'keycloak-angular';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideKeycloak, includeBearerTokenInterceptor, INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG } from 'keycloak-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,13 @@ export const appConfig: ApplicationConfig = {
       initOptions: {
         onLoad: 'login-required'
       }
-    })
+    }),
+    {
+      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+      useValue: [{ urlPattern: /^(http:\/\/localhost:9090)(\/.*)?$/ }]
+    },
+    provideHttpClient(
+      withInterceptors([includeBearerTokenInterceptor])
+    )
   ]
 };
